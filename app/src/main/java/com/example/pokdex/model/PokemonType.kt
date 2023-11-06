00842dcd93a69fa1e4cb1c6c130bcd79520d7249
Smig <ahmedharoun370@gmail.com>
2023-11-06 13:22:55 -0500

    + PokemonType enum class to determine pokemon types (Grass, Fire, Etc.) + Refactored PokemonInformationCard() Composable into two separate functions   ->PokemonImage() Displays image of the pokemon   ->PokemonInformation() Displays some pokemon information
    + Added ditto_front_default_sample.png for previewing composable functions

---
+++ app/src/main/java/com/example/pokdex/model/PokemonType.kt
@@ -0,0 +1,21 @@
+package com.example.pokdex.model
+
+enum class PokemonType {
+    NORMAL,
+    BUG,
+    FIGHT,
+    GHOST,
+    ELECTRIC,
+    FLYING,
+    STEEL,
+    PSYCHIC,
+    POISON,
+    FIRE,
+    ICE,
+    GROUND,
+    WATER,
+    DRAGON,
+    ROCK,
+    GRASS,
+    DARK
+}
--- app/src/main/java/com/example/pokdex/ui/PokemonListScreen.kt
+++ app/src/main/java/com/example/pokdex/ui/PokemonListScreen.kt
@@ -9,8 +9,6 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.Card
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
@@ -29,6 +27,7 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.pokdex.R
+import com.example.pokdex.model.PokemonType
 
 @Composable
 fun PokemonListScreen(modifier: Modifier){
@@ -66,11 +65,14 @@ fun PokemonInformationCard(
     modifier: Modifier = Modifier,
     @DrawableRes pokemonImage: Int,
     pokemonName: String,
-    pokemonNumber: Int,
-    showFemaleSymbol: Boolean = true,
-    showMaleSymbol: Boolean = true,
-    onSaved: ()->Unit,
-    saved: Boolean = false
+    pokemonIndex: Int,
+    showFemaleSymbol: Boolean,
+    showMaleSymbol: Boolean,
+    onPokemonSaved: ()->Unit,
+    isPokemonSaved: Boolean,
+    pokemonFirstType: PokemonType,
+    pokemonSecondType: PokemonType? = null,
+    pokemonThirdType: PokemonType? = null
 ){
     Card(
         shape = MaterialTheme.shapes.large,
@@ -79,52 +81,20 @@ fun PokemonInformationCard(
             .height(100.dp)
     ){
         Row(
-            modifier = Modifier.padding(dimensionResource(id = R.dimen.small_padding))
+            modifier = Modifier
+                .padding(dimensionResource(id = R.dimen.small_padding))
         ) {
-            Image(
-                painter = painterResource(id = pokemonImage),
-                contentDescription = null,
-                contentScale = ContentScale.Crop,
-                modifier = Modifier.
-                    clip(MaterialTheme.shapes.medium)
-            )
+            PokemonImage(pokemonImageId = pokemonImage)
 
-            Column {
-                Text(
-                    text = pokemonName,
-                    style = MaterialTheme.typography.titleLarge,
-                    modifier = Modifier
-                        .padding(
-                            start = dimensionResource(id = R.dimen.medium_padding),
-                            end = dimensionResource(id = R.dimen.small_padding)
-                        )
-                )
-            }
-
-            Icon(
-                painter = painterResource(id = R.drawable.male_symbol_icon),
-                contentDescription = "Male symbol",
-                tint = Color.Blue,
+            PokemonInformation(
+                pokemonName = pokemonName,
+                showFemaleSymbol = showFemaleSymbol,
+                showMaleSymbol = showMaleSymbol,
+                pokemonIndex = pokemonIndex,
+                onPokemonSaved = onPokemonSaved,
+                isPokemonSaved = isPokemonSaved,
+                pokemonFirstType = pokemonFirstType
             )
-            Icon(
-                painter = painterResource(id = R.drawable.female_symbol_icon),
-                contentDescription = "Female symbol",
-                tint = Color.Magenta,
-            )
-
-            Column(
-                horizontalAlignment = Alignment.End,
-                verticalArrangement = Arrangement.Bottom,
-                modifier = Modifier.fillMaxSize()
-            ) {
-                Text(
-                    text = "#$pokemonNumber",
-                    style = MaterialTheme.typography.titleLarge,
-                    fontWeight = FontWeight.ExtraBold,
-                    modifier = Modifier.alpha(0.6f)
-                )
-                HeartSaveButton(onClick = { onSaved() }, saved = saved )
-            }
 
         }
     }
@@ -137,9 +107,128 @@ fun PokemonInformationCard(
 fun PokemonInformationCardPreview(){
     PokemonInformationCard(
         modifier = Modifier,
-        pokemonImage = R.drawable.ic_launcher_background,
-        pokemonName = "Chandler",
-        pokemonNumber = 5,
-        onSaved = {},
+        pokemonImage = R.drawable.ditto_front_default_sample,
+        pokemonName = "Ditto",
+        pokemonIndex = 234,
+        showFemaleSymbol = true,
+        showMaleSymbol = true,
+        onPokemonSaved = {},
+        isPokemonSaved = true,
+        pokemonFirstType = PokemonType.NORMAL
     )
+}
+
+
+
+
+
+
+
+@Composable
+fun PokemonImage(
+    modifier: Modifier = Modifier,
+    @DrawableRes pokemonImageId: Int
+    ){
+    
+    Image(
+        painter = painterResource(id = pokemonImageId),
+        contentDescription = null,
+        contentScale = ContentScale.Fit,
+        modifier = Modifier
+            .clip(MaterialTheme.shapes.medium)
+    )
+    
+}
+
+@Preview(
+    name = "PokemonImage Preview",
+)
+@Composable
+fun PokemonImagePreview(){
+    PokemonImage(pokemonImageId = R.drawable.ditto_front_default_sample)
+}
+
+
+
+
+
+
+
+
+@Composable
+fun PokemonInformation(
+    modifier: Modifier = Modifier,
+    pokemonName: String,
+    showFemaleSymbol: Boolean,
+    showMaleSymbol: Boolean,
+    pokemonIndex: Int,
+    onPokemonSaved: () -> Unit,
+    isPokemonSaved: Boolean,
+    /* Pokemon are known to have a maximum of three types
+     * Variables also determine the order in which types are shown */
+    pokemonFirstType: PokemonType,
+    pokemonSecondType: PokemonType? = null,
+    pokemonThirdType: PokemonType? = null,
+){
+
+    Row() {
+        Column {
+            Text(
+                text = pokemonName,
+                style = MaterialTheme.typography.headlineMedium,
+                color = Color.Black
+            )
+
+            /* Add a switch statement to determine the pokemon's type
+             * and display that types image */
+        }
+
+        if(showFemaleSymbol){
+            Icon(
+                painter = painterResource(id = R.drawable.female_symbol_icon),
+                tint = Color.Magenta,
+                contentDescription = "Female symbol"
+            )
+        }
+        if(showMaleSymbol){
+            Icon(
+                painter = painterResource(id = R.drawable.male_symbol_icon),
+                tint = Color.Blue,
+                contentDescription = "Male symbol"
+            )
+        }
+
+        Column(
+            horizontalAlignment = Alignment.End,
+            verticalArrangement = Arrangement.Top
+        ) {
+            Text(
+                text = "#$pokemonIndex",
+                style = MaterialTheme.typography.headlineSmall,
+                fontWeight = FontWeight.ExtraBold,
+                modifier = Modifier.alpha(0.6f)
+            )
+            HeartSaveButton(onClick = { onPokemonSaved() }, saved = isPokemonSaved)
+
+        }
+
+    }
+
+}
+
+@Preview(
+    name = "PokemonInformation Preview"
+)
+@Composable
+fun PokemonInformationPreview(){
+    PokemonInformation(
+        modifier = Modifier,
+        pokemonName = "Ditto",
+        showFemaleSymbol = true,
+        showMaleSymbol = true,
+        pokemonIndex = 123,
+        onPokemonSaved = {},
+        isPokemonSaved = true,
+        pokemonFirstType = PokemonType.NORMAL
+        )
 }
