8a783265b2cc717c677f42e042e885674345f423
Smig <ahmedharoun370@gmail.com>
2023-11-19 09:37:23 -0500

    Condensed Viewmodel State into a single uiState

--- app/src/main/java/com/example/pokdex/Resource.kt
+++ app/src/main/java/com/example/pokdex/Resource.kt
@@ -2,6 +2,8 @@ package com.example.pokdex
 
 sealed class Resource<out T> {
     data class Success<out T>(val data: T) : Resource<T>()
-    data class Loading<out T>(val data: T? = null) : Resource<T>()
+
+    // Don't need this for our remote data source, we either succeed in our request of fail
+    //data class Loading<out T>(val data: T? = null) : Resource<T>()
     data class Error<out T>(val message: String, val data: T? = null) : Resource<T>()
 }
--- app/src/main/java/com/example/pokdex/ui/screens/PokemonListScreenViewModel.kt
+++ app/src/main/java/com/example/pokdex/ui/screens/PokemonListScreenViewModel.kt
@@ -1,51 +1,74 @@
 package com.example.pokdex.ui.screens
 
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.pokdex.Constants
+import com.example.pokdex.Resource
 import com.example.pokdex.data.PokemonRepository
 import com.example.pokdex.model.PokemonListEntry
 import kotlinx.coroutines.launch
 
+data class uiState(
+    var pokemonList: List<PokemonListEntry> = listOf(),
+    var loadingErrorString: String = "",
+    var loadingError: Boolean = false,
+    var isLoading: Boolean = false,
+    var endReached: Boolean = false
+)
 
 class PokemonListScreenViewModel(
     private val repository: PokemonRepository
 ): ViewModel() {
 
     private var currentPage = 0
-
-    var pokemonList = mutableStateOf<List<PokemonListEntry>>(listOf())
-    var loadingError = mutableStateOf("")
-    var isLoading = mutableStateOf(false)
-    var endReached = mutableStateOf(false)
+    var uiState by mutableStateOf(uiState())
+    private set
 
     init {
         loadPokemonPaginated()
     }
     fun loadPokemonPaginated(){
+
         viewModelScope.launch {
-            isLoading.value = true
+
+            uiState.isLoading = true
             val result = repository.getPokemonList(Constants.PAGE_SIZE, currentPage * Constants.PAGE_SIZE)
-            if(currentPage * Constants.PAGE_SIZE >= result.count) {
-                endReached.value = true
-            }
-            val pokemonEntries = result.results.mapIndexed{index, entry->
-                val number = if(entry.url.endsWith("/")){
-                    entry.url.dropLast(1).takeLastWhile { it.isDigit() }
-                } else {
-                    entry.url.takeLastWhile { it.isDigit() }
+
+            when(result){
+
+                is Resource.Success->{
+                    if(currentPage * Constants.PAGE_SIZE >= result.data.count) {
+                        uiState.endReached = true
+                    }
+                    val pokemonEntries = result.data.results.mapIndexed{index, entry->
+                        val number = if(entry.url.endsWith("/")){
+                            entry.url.dropLast(1).takeLastWhile { it.isDigit() }
+                        } else {
+                            entry.url.takeLastWhile { it.isDigit() }
+                        }
+                        val url = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/${number}.png"
+                        PokemonListEntry(
+                            pokemonName = entry.name,
+                            pokemonImageUrl = url
+                        )
+                    }
+                    currentPage++
+                    uiState.loadingErrorString = ""
+                    uiState.loadingError = false
+                    uiState.isLoading = false
+                    uiState.pokemonList += pokemonEntries
                 }
-                val url = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/${number}.png"
-                PokemonListEntry(
-                    pokemonName = entry.name,
-                    pokemonImageUrl = url
-                )
+
+                is Resource.Error->{
+                    uiState.loadingErrorString = result.message
+                    uiState.loadingError= true
+                }
             }
-            currentPage++
-            loadingError.value = ""
-            isLoading.value = false
-            pokemonList.value += pokemonEntries
+
         }
     }
 
