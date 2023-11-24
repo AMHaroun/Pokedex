5b4e338f02fc6f654daab18e266a335bec8317ac
Smig <ahmedharoun370@gmail.com>
2023-11-24 16:00:35 -0500

    Updated Launcher Icons

--- app/src/main/java/com/example/pokdex/ui/screens/PokemonDetailScreen.kt
+++ app/src/main/java/com/example/pokdex/ui/screens/PokemonDetailScreen.kt
@@ -1,16 +1,18 @@
 package com.example.pokdex.ui.screens
 
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 
 @Composable
 fun PokemonDetailScreen(
     pokemonName: String,
+    viewModel: PokemonDetailScreenViewModel = viewModel(factory = PokemonDetailScreenViewModel.Factory),
     navController: NavController,
     modifier: Modifier = Modifier
 ){
+
 
 
 }
