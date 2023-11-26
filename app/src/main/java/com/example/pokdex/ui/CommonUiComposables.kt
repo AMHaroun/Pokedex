ca056b8f6b5cd182c88c29fe4eb7bd7d7164c21e
Smig <ahmedharoun370@gmail.com>
2023-11-26 07:45:11 -0500

    Moved NetworkErrorMessage() & LoadingSpinner() Composables into CommonUiComposables.kt

--- app/src/main/java/com/example/pokdex/ui/CommonUiComposables.kt
+++ app/src/main/java/com/example/pokdex/ui/CommonUiComposables.kt
@@ -1,12 +1,14 @@
 package com.example.pokdex.ui
 
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.FavoriteBorder
 import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Button
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
@@ -17,6 +19,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.input.ImeAction
@@ -89,5 +92,59 @@ fun SearchBarPreview(){
 }
 
 
+@Composable
+fun NetworkErrorMessage(
+    modifier: Modifier = Modifier,
+    errorMessage: String? = null,
+    onRetry:()->Unit
+){
+
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier
+    ) {
+
+        Text(
+            text = errorMessage ?: "A network error has occurred",
+            style = MaterialTheme.typography.labelLarge
+        )
+
+        Button(onClick = { onRetry }) {
+            Text(text = "Retry")
+        }
+
+    }
+
+}
+
+@Preview(
+    name = "NetworkErrorMessage Preview",
+    showBackground = true
+)
+@Composable
+fun NetworkErrorMessagePreview(){
+
+    NetworkErrorMessage(errorMessage = null) {}
+
+}
+
+
+
+@Composable
+fun LoadingSpinner(modifier: Modifier = Modifier){
+
+    CircularProgressIndicator(modifier = modifier)
+}
+
+@Preview(
+    name = "LoadingSpinner Preview",
+    showBackground = true
+)
+@Composable
+fun LoadingSpinnerPreview(){
+
+    LoadingSpinner()
+
+}
 
 
--- app/src/main/java/com/example/pokdex/ui/screens/PokemonDetailScreen.kt
+++ app/src/main/java/com/example/pokdex/ui/screens/PokemonDetailScreen.kt
@@ -2,17 +2,14 @@ package com.example.pokdex.ui.screens
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.material3.Button
-import androidx.compose.material3.CircularProgressIndicator
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
+import com.example.pokdex.ui.LoadingSpinner
+import com.example.pokdex.ui.NetworkErrorMessage
 
 @Composable
 fun PokemonDetailScreen(
@@ -51,60 +48,6 @@ fun PokemonDetailScreen(
 
     }
 
-
-}
-
-// TODO: move this to CommonUiComposables.kt
-@Composable
-fun NetworkErrorMessage(
-    modifier: Modifier = Modifier,
-    errorMessage: String? = null,
-    onRetry:()->Unit
-){
-
-    Column(horizontalAlignment = Alignment.CenterHorizontally) {
-
-        Text(
-            text = errorMessage ?: "A network error has occurred",
-            style = MaterialTheme.typography.labelLarge
-        )
-
-        Button(onClick = { onRetry }) {
-            Text(text = "Retry")
-        }
-
-    }
-
-}
-
-@Preview(
-    name = "NetworkErrorMessage Preview",
-    showBackground = true
-)
-@Composable
-fun NetworkErrorMessagePreview(){
-
-    NetworkErrorMessage(errorMessage = null){}
-
-}
-
-
-
-// TODO: Move this to CommonUiComposables.kt
-@Composable
-fun LoadingSpinner(modifier: Modifier = Modifier){
-
-    CircularProgressIndicator(modifier = modifier)
-}
-
-@Preview(
-    name = "LoadingSpinner Preview",
-    showBackground = true
-)
-@Composable
-fun LoadingSpinnerPreview(){
-
-    LoadingSpinner()
 
 }
 
