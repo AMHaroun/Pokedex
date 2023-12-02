ed050600ae9a162cc36ffda4e35a37e34168a5e1
Smig <ahmedharoun370@gmail.com>
2023-12-02 13:05:06 -0500

    Added enter & exit transitions for the PokemonDetailScreen() Composable in NavHost()

--- app/src/main/java/com/example/pokdex/ui/PokedexApp.kt
+++ app/src/main/java/com/example/pokdex/ui/PokedexApp.kt
@@ -1,9 +1,12 @@
 package com.example.pokdex.ui
 
+import androidx.compose.animation.core.LinearOutSlowInEasing
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.slideInHorizontally
+import androidx.compose.animation.slideOutHorizontally
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.toLowerCase
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -19,12 +22,34 @@ fun PokedexApp(
 ){
     val navController = rememberNavController()
     
-    NavHost(navController = navController, startDestination = "PokemonListScreen"){
-        composable("PokemonListScreen"){
+    NavHost(
+        navController = navController,
+        startDestination = "PokemonListScreen",
+        modifier = modifier
+    ){
+        composable(route = "PokemonListScreen"){
             PokemonListScreen(navController = navController)
         }
         composable(
-            "PokemonDetailScreen/{pokemonName}",
+            exitTransition ={
+                slideOutHorizontally(
+                    animationSpec = tween(
+                        durationMillis = 500,
+                        easing = LinearOutSlowInEasing
+                    ),
+                    targetOffsetX = { it }
+                )
+            },
+            enterTransition = {
+                slideInHorizontally(
+                    animationSpec = tween(
+                        durationMillis = 700,
+                        easing = LinearOutSlowInEasing
+                    ),
+                    initialOffsetX = { it }
+                )
+            },
+            route = "PokemonDetailScreen/{pokemonName}",
             arguments = listOf(
                     navArgument("pokemonName") {
                         type = NavType.StringType
