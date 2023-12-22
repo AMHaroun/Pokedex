ae4600eff46bf8d1abd157fd554aab18f4174016
Smig <ahmedharoun370@gmail.com>
2023-12-22 16:09:39 -0500

    Setup testing for PokemonRepositoryImpl
    Created FakePokemonApiService modeling responses from the api
    Created PokemonRepositoryImplTest class and implemented setUp function

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -81,7 +81,11 @@ dependencies {
     implementation("androidx.hilt:hilt-navigation-compose:1.1.0")
     kapt("com.google.dagger:hilt-android-compiler:2.49")
 
+    // Local unit tests
+    testImplementation("com.google.truth:truth:1.1.3")
     testImplementation("junit:junit:4.13.2")
+
+    //Instrumentation tests
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
