ce453d82291c25c0267ad6678894a57c59f48479
Smig <ahmedharoun370@gmail.com>
2023-12-24 10:41:39 -0500

    Added searchBar_isClickable test case

--- app/src/androidTest/java/com/example/pokdex/ui/screens/PokemonListScreenTest.kt
+++ app/src/androidTest/java/com/example/pokdex/ui/screens/PokemonListScreenTest.kt
@@ -1,12 +1,15 @@
 package com.example.pokdex.ui.screens
 
 import androidx.activity.compose.setContent
+import androidx.compose.ui.test.assertHasClickAction
+import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
 import androidx.compose.ui.test.onNodeWithTag
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.pokdex.MainActivity
+import com.example.pokdex.util.TestTags
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
 import org.junit.Before
@@ -37,6 +40,14 @@ class PokemonListScreenTest{
 
     @Test
     fun searchBar_isVisible(){
-        composeRule.onNodeWithTag("SEARCH_BAR").assertExists()
+        composeRule.onNodeWithTag(TestTags.SEARCH_BAR).assertIsDisplayed()
     }
+
+    @Test
+    fun searchBar_isClickable(){
+        composeRule.onNodeWithTag(TestTags.SEARCH_BAR).assertHasClickAction()
+    }
+
+
+
 }
